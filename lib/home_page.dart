6521d1eb511ffbb392354a6174c004452dc53066
MD-AbdullaAlMasudHimel <himel.bubt.cse@gmail.com>
2021-07-20 10:26:05 +0600

    day one done

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int day = 30;
+    final String name = "Himel";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $day of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(
+
+      ),
+    );
+  }
+}
