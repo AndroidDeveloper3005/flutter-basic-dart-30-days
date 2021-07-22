7512fe59eac97ce3008378089ddd385104d33dad
MD-AbdullaAlMasudHimel <himel.bubt.cse@gmail.com>
2021-07-22 12:51:26 +0600

    all day three work done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/pages/login_page.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -11,7 +12,20 @@ class MyApp extends StatelessWidget {
     bringFish(bag: true, taka: 50);
     bringCat(bag: true, taka: 120);
 
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      //home: HomePage(),
+      //theme
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      //routes
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 
   bringFish({bool bag = false, int taka = 100}) {
